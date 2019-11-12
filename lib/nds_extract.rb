2a0fd921984e4291bd2c48ea421f541a50aca283
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-12 18:49:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -60,7 +60,7 @@ def movies_with_director_key(name, movies_collection)
    aoh.push(movie_with_director_name(name, movies_collection[i]))
     i += 1
   end
-
+binding.pry
 aoh
 
 end
@@ -127,8 +127,6 @@ source.each do |director_info|
 
           movie_info = {:title => movie[:title], :director_name => director_info[:name]}
           result.push([movie_info])
-
-          binding.pry
 
       end
 
